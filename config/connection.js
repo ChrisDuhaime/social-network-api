d6e4b2097ba39cd921a799b6cfac8cfdce4598bd
Chris Duhaime <c.duhaime27@gmail.com>
2022-07-27 11:09:27 -0400

    imported recent class assn to help get updated file structure, swapped models

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
-
-connect(connectionString, {
+connect('mongodb://localhost/videosAndResponses', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
